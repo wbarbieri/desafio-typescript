933aa7611885f7b959531fe86c4ff107195c29a3
Wendell Barbieri <wendell.t.barbieri@gmail.com>
2022-06-09 17:49:04 -0300

    typescript-dio

---
+++ desafio3.js
@@ -0,0 +1,45 @@
+
+
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+
+let valorTotal = 0;
+limparSaldo();
+
+function somarAoSaldo(soma) {
+
+    if( campoSaldo ){   
+    valorTotal += soma;
+    campoSaldo.innerHTML = valorTotal.toString();
+    limparSoma();
+    }
+}
+
+
+function limparSaldo() {
+    if( campoSaldo ){
+    valorTotal = 0;
+    campoSaldo.innerHTML = valorTotal.toString();
+    }
+}
+
+if(botaoAtualizar){
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+}
+
+if(botaoLimpar){
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+}
+
+
+function limparSoma() {
+    soma.value = '';
+}
+
+
